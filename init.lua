cb0ba8793baa3f9781b314c6079451431751e043
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-08 02:57:25 +0300

    feat: configure harpoon plugin

--- init.lua
+++ init.lua
@@ -73,6 +73,13 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      require('harpoon.tabline').setup({});
+      require('telescope').load_extension('harpoon');
+    end,
+  },
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
