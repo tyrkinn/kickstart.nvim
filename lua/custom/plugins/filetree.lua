440da7c7bbfb0a4c2917c972846ed0c18e624871
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-08 21:54:03 +0300

    fix: update neotree bindings

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,8 +9,9 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     require('neo-tree').setup {}
-    vim.keymap.set('n', '<leader>0', ':Neotree<CR>')
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
+    vim.keymap.set('n', '<leader>0', ':Neotree toggle<CR>')
   end,
 }
